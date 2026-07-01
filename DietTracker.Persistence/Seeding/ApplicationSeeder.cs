d9a75de5829cf84f0f095a6878db83a6f6bfc131
phario <grzedyn@gmail.com>
2026-07-01 23:34:29 +0200

    init seeder

---
+++ DietTracker.Persistence/Seeding/ApplicationSeeder.cs
@@ -0,0 +1,9 @@
+namespace DietTracker.Persistence.Seeding;
+
+public class ApplicationSeeder
+{
+    public static async Task SeedAsync(DietTrackerDbContext context, CancellationToken ct)
+    {
+        
+    }
+}
